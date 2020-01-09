d9de29fba6a982dc31ced641c6af4e487954ee7d
youyoujiushishuang <497706870@qq.com>
2020-01-09 15:29:55 +0800

    使用iconfont图标(已在iconfont官网建立此项目,可更新图标,然后下载至本地,解压后,将 样式文件中的iconfonts中内容替换)

---
+++ yjs-angular/src/app/areas/demo/demo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DemoRoutingModule } from './demo-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DemoRoutingModule
+  ],
+  declarations: []
+})
+export class DemoModule { }
